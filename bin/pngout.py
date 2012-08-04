cf28e73af71f2b38b61def68ef50534127819de9
maqian <maqian.cn@gmail.com>
2012-08-04 13:52:57 +0800

    update

--- bin/pngout.py
+++ bin/pngout.py
@@ -2,9 +2,10 @@ import sys
 import os
 if len(sys.argv) < 2:
 	print "please input the path you want to compress"
-	sys.exit()
-for root, dirs, files in os.walk(sys.argv[1]):
-	for filespath in files:
-		fullpath = os.path.join(root,filespath)
-		if fullpath.endswith(".png"):
-			os.system("pngout-win32.exe"+" "+fullpath)
+	sys.exit(-1)
+for path in sys.argv[1:]:
+	for root, dirs, files in os.walk(path):
+		for filespath in files:
+			fullpath = os.path.join(root,filespath)
+			if fullpath.endswith(".png"):
+				os.system("pngout-win32.exe"+" "+fullpath)
